fa364102a3bd726acb52eceea92042bcafdc7e96
Babangida Tsowa <tsowababangida@gmail.com>
2024-06-02 22:45:04 +0100

    Init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import RootLayout from '../layouts/root'
+import "../styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-poppins",
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+ 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={poppins.className}>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </div>
+  )
+}
