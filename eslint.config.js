35e7d6c612a08051d9b9485fa6c6ea6abae1784f
Babangida Tsowa <tsowababangida@gmail.com>
2024-06-03 00:05:33 +0100

    Added: ESlint, Husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,51 @@
-export default {
-    "env": {
-        "browser": true,
-        "es2021": true
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import airbnb from "eslint-config-airbnb";
+import airbnbHooks from "eslint-config-airbnb/hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+
+export default [
+  // airbnb,
+  // airbnbHooks.rules,
+  // jsxA11y.configs,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+    //   // ...airbnb.rules,
+    //   'no-console': 'warn',
+    //   '@typescript-eslint/no-explicit-any': 'error',
+    //   // '@typescript-eslint/no-empty-function': 'on',
+    //   // '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/semi': 'off',
+      '@typescript-eslint/quotes': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'react/react-in-jsx-scope': 'off',
+    //   'react/jsx-uses-react': 'off',s
+    //   // 'import/extensions': [
+    //   //   'error',
+    //   //   'ignorePackages',
+    //   //   {
+    //   //     js: 'never',
+    //   //     jsx: 'never',
+    //   //     ts: 'never',
+    //   //     tsx: 'never',
+    //   //   },
+    //   // ],
     },
-    "extends": [
-        "airbnb",
-        "airbnb/hooks",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "no-console": "warn",
-        "@typescript-eslint/no-explicit-any": "error",
-        "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
-        "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-              "js": "never",
-              "jsx": "never",
-              "ts": "never",
-              "tsx": "never"
-            }
-        ],
-    },
-    "settings": {
-        "import/resolver": {
-          "node": {
-            "extensions": [".js", ".jsx", ".ts", ".tsx"]
-          }
-        }
-    }
-}
+  }
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import s from './index.module.css';
+import Image from 'next/image'
+// import s from './index.module.css'
 
 const Home = () => {
   return (
